6fb2961ed65590daf0b145cc78f50d8689d3077b
karthik.shekar <karthikshekar237@gmail.com>
2025-03-25 12:10:12 +1100

    removed files

--- terraform/backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket         = "tfstatemoneksbucket"
-    key            = "eks/terraform.tfstate"
-    region         = "ap-southeast-2"
-    dynamodb_table = "tf-lock-monitoring-dev"
-    encrypt        = true
-  }
-}
